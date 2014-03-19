8e733b846a4609cca86288be7ab6dca07c16ddb5
Paresh Ghodge <psghodge@gmail.com>
2014-03-19 13:49:33 -0500

    initial commit (created application folder)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NEMT::Application.config.secret_key_base = '843554fbcf3ec8e49a07a7201efd8b4f4c01c3447aa1b09c068ff3dcb7bcad6a19782b1e3a7ff70c91002872d7e44bd30b9697e147496307ceb88bdb0aa0c464'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NEMT::Application.config.session_store :cookie_store, key: '_NEMT_session'
